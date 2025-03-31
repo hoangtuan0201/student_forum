7173b486c972584741cae37e7c65295cc77fdda2
PC <hoangtuan0796991061@gmail.com>
2025-03-31 23:06:40 +0700

    Enhance UI for better role visibility.

--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -4,13 +4,49 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 ?>
 
-
 <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container">
         <a class="navbar-brand" href="/student_forum/public/index.php">Student Forum</a>
+        
+        <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin'): ?>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/student_forum/admin/index.php">
+                            <i class="fas fa-tachometer-alt"></i> Dashboard
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/student_forum/admin/users.php">
+                            <i class="fas fa-users"></i> Users
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/student_forum/admin/posts.php">
+                            <i class="fas fa-file-alt"></i> Posts
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/student_forum/admin/modules.php">
+                            <i class="fas fa-book"></i> Modules
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        <?php endif; ?>
+
         <div>
             <?php if (isset($_SESSION['username'])): ?>
-                <span class="navbar-text mr-3">Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?></span>
+                <span class="navbar-text mr-3">
+                    Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>
+                    <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin'): ?>
+                        <span class="badge badge-warning">Admin</span>
+                    <?php endif; ?>
+                </span>
                 <a href="/student_forum/app/controllers/AuthController.php?action=logout" class="btn btn-outline-light">Logout</a>
             <?php else: ?>
                 <a href="/student_forum/app/views/auth/login.php" class="btn btn-outline-light mr-2">Login</a>
