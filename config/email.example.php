a2ad14b0b2de229ba72b2f6d7c8ba45b68e8ded3
PC <hoangtuan0796991061@gmail.com>
2025-04-20 15:02:46 +0700

    Add .gitignore and email.example.php template

---
+++ config/email.example.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'smtp' => [
+        'host' => 'smtp.gmail.com', // Your SMTP host
+        'port' => 587,
+        'username' => '', // Your email address
+        'password' => '', // Your email password/app password
+        'encryption' => 'tls',
+        'from_email' => 'no-reply@studentforum.com',
+        'from_name' => 'Student Forum'
+    ]
+];
+?>
