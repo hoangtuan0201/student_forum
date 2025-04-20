8cca79f773ea840dfc4b55937f92dc7e26714a89
hoangtuan0201 <145053153+hoangtuan0201@users.noreply.github.com>
2025-04-20 12:59:14 +0700

    Update email.php

--- config/email.php
+++ config/email.php
@@ -4,7 +4,7 @@ return [
         'host' => 'smtp.gmail.com', // Change this to your SMTP host
         'port' => 587,
         'username' => 'tuanthhgcs230462@fpt.edu.vn', // Change this to your email
-        'password' => 'fpey xdhr cutu irai', // Change this to your app password
+        'password' => '', // Change this to your app password
         'encryption' => 'tls',
         'from_email' => 'no-reply@studentforum.com',
         'from_name' => 'Student Forum'
